9c9fc8bf9234b0870fbf6570013bc989b8a6d80c
UtopiaEH <utopiaeh01@gmail.com>
2022-02-17 11:09:24 +0200

    added posibility to change the name for download file pdf type

--- src/components/FileName.tsx
+++ src/components/FileName.tsx
@@ -1,39 +1,45 @@
-import React, { FC, useContext } from "react";
-import styled from "styled-components";
-import { DocViewerContext } from "../state";
-import { IStyledProps } from "../types";
+import React, { FC, useContext } from 'react'
+import styled from 'styled-components'
+import { DocViewerContext } from '../state'
+import { IStyledProps } from '../types'
 
 export const FileName: FC<{}> = () => {
-  const {
-    state: { config, currentDocument },
-  } = useContext(DocViewerContext);
+    const {
+        state: { config, currentDocument }
+    } = useContext(DocViewerContext)
 
-  if (!currentDocument || config?.header?.disableFileName) return null;
+    if (!currentDocument || config?.header?.disableFileName) return null
 
-  let fileName = currentDocument.uri || "";
-  fileName = decodeURI(fileName);
+    let fileName = ''
 
-  if (!config?.header?.retainURLParams) {
-    fileName = fileName.split("?")[0];
-  }
+    if (currentDocument.fileName) {
+        fileName = currentDocument.fileName
+    } else {
+        fileName = currentDocument.uri || ''
+        fileName = decodeURI(fileName)
 
-  const splitURL = fileName.split("/");
-  if (splitURL.length) {
-    fileName = splitURL[splitURL.length - 1];
-  }
+        if (!config?.header?.retainURLParams) {
+            fileName = fileName.split('?')[0]
+        }
 
-  return (
-    <Container id="file-name" data-testid="file-name">
-      {fileName}
-    </Container>
-  );
-};
+        const splitURL = fileName.split('/')
+        if (splitURL.length) {
+            fileName = splitURL[splitURL.length - 1]
+        }
+    }
+
+    return (
+        <Container id='file-name' data-testid='file-name'>
+            { fileName }
+        </Container>
+    )
+}
 
 const Container = styled.div`
   flex: 1;
   text-align: left;
-  color: ${(props: IStyledProps) => props.theme.text_primary};
+  color: ${ (props: IStyledProps) => props.theme.text_primary };
   font-weight: bold;
   margin: 0 10px;
   overflow: hidden;
-`;
+`
