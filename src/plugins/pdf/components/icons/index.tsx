65957735ee09207bd278c594020eed055ec5e57e
Marin Pesterean <marin.pesterean@carasent.com>
2024-02-22 12:50:37 +0200

    1.4

--- src/plugins/pdf/components/icons/index.tsx
+++ src/plugins/pdf/components/icons/index.tsx
@@ -154,13 +154,15 @@ export const TogglePaginationPDFIcon = (props: IIconProps) => {
 export const PrintPDFIcon = (props: IIconProps) => {
   const { color, size, reverse } = props;
   return (
-    <svg xmlns="http://www.w3.org/2000/svg"  
-    width={size || "100%"}
-    height={size || "100%"}
-    style={{ transform: `${reverse ? "rotate(180deg)" : ""}` }}
-    id="Layer_1"
-    viewBox="0 0 512 512">
-      <path d="M128 0C92.7 0 64 28.7 64 64v96h64V64H354.7L384 93.3V160h64V93.3c0-17-6.7-33.3-18.7-45.3L400 18.7C388 6.7 371.7 0 354.7 0H128zM384 352v32 64H128V384 368 352H384zm64 32h32c17.7 0 32-14.3 32-32V256c0-35.3-28.7-64-64-64H64c-35.3 0-64 28.7-64 64v96c0 17.7 14.3 32 32 32H64v64c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V384zM432 248a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
+    <svg
+      width={size || "100%"}
+      height={size || "100%"}
+      style={{ transform: `${reverse ? "rotate(180deg)" : ""}` }}
+      id="Layer_1"
+      viewBox="0 0 512 512"
+    >
+      <path d="M128 0C92.7 0 64 28.7 64 64v96h64V64H354.7L384 93.3V160h64V93.3c0-17-6.7-33.3-18.7-45.3L400 18.7C388 6.7 371.7 0 354.7 0H128zM384 352v32 64H128V384 368 352H384zm64 32h32c17.7 0 32-14.3 32-32V256c0-35.3-28.7-64-64-64H64c-35.3 0-64 28.7-64 64v96c0 17.7 14.3 32 32 32H64v64c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V384zM432 248a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
+    </svg>
   );
 };
 
